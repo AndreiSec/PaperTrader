c9ec99d5fe6aee1acb12e9bc633f02b5b32b8765
AndreiSec <andreisecara01@gmail.com>
2021-10-22 19:22:45 -0400

    Added Login Screen

--- app/src/main/java/com/example/papertrader/EntryActivity.java
+++ app/src/main/java/com/example/papertrader/EntryActivity.java
@@ -2,13 +2,37 @@ package com.example.papertrader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.papertrader.ui.login.LoginActivity;
 
 public class EntryActivity extends AppCompatActivity {
+
+    private Button signin_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+        signin_button = (Button) findViewById(R.id.button_signin);
+        signin_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSignInActivity();
+
+            }
+
+        });
     }
+
+    public void openSignInActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/papertrader/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/papertrader/ui/login/LoginActivity.java
@@ -23,11 +23,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.papertrader.R;
-import com.example.papertrader.ui.login.LoginViewModel;
-import com.example.papertrader.ui.login.LoginViewModelFactory;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private FloatingActionButton button_back;
     private LoginViewModel loginViewModel;
 
     @Override
@@ -39,8 +38,18 @@ public class LoginActivity extends AppCompatActivity {
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
+        final Button loginButton = findViewById(R.id.button_login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+        button_back = (FloatingActionButton) findViewById(R.id.button_back_loginscreen);
+        button_back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish(); // Ends activity and returns to the previous
+
+            }
+        });
+
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
